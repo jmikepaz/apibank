56ac0badb57b198836809d386fe798c121987cea
JUANMIGUELPAZ\migue <juanmpaz2@gmail.com>
2021-11-06 13:14:56 -0600

    recover

---
+++ Usuarios/recover_password.php
@@ -0,0 +1,33 @@
+<?php 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: *");
+include "../conexion.php";
+$postdata = file_get_contents("php://input");  
+
+$sql_query = "SELECT  public.ft_proc_recovery_account('".$postdata."')";
+
+if (pg_send_query($con, $sql_query)) {
+    $res=pg_get_result($con);
+    if ($res) {
+      $state = pg_result_error_field($res, PGSQL_DIAG_SQLSTATE);
+      if ($state==0) {
+            $datos = pg_fetch_all($res);
+            $variable = $datos[0]["ft_proc_recovery_account"]; 
+            echo  $variable ;    
+            }
+      else {
+        // some error happened
+        if ($state == 23505) { // unique_violation
+            $resultado = array('estado' => $state);
+            echo json_encode($resultado) ;    
+        }
+        else {
+            $resultado = array('estado' => $state);
+            echo json_encode($resultado) ;
+         // process other errors
+        }
+      }
+    }  
+  }
+
+?>
