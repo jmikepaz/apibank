d6038d19fd921fecd1cca769317e3460500a3b50
JUANMIGUELPAZ\migue <juanmpaz2@gmail.com>
2021-11-07 09:15:54 -0600

    email

--- send.php
+++ send.php
@@ -1,7 +1,8 @@
 <?php 
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: *");
-
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
 $to = "soporte@solucioneseconomicashn.com";
 $subject = "Asunto del email";
 $message = "Este es mi primer envío de email con PHP";
